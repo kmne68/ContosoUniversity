bb32311f13174f4b10206248f4ce9ea76546a005
kmne68 <kmne68@gmail.com>
2016-10-16 02:12:38 -0500

    Complete CRUD addition

--- ContosoUniversity/Controllers/StudentController.cs
+++ ContosoUniversity/Controllers/StudentController.cs
@@ -68,24 +68,53 @@ namespace ContosoUniversity.Controllers
         }
 
         // GET: Student/Edit/5
-        public ActionResult Edit(int? id)
+                      public ActionResult Edit(int? id)
+                      {
+                          if (id == null)
+                          {
+                              return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                          }
+                          Student student = db.Students.Find(id);
+                          if (student == null)
+                          {
+                              return HttpNotFound();
+                          }
+                          return View(student);
+                      }
+                      
+
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditPost(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Student student = db.Students.Find(id);
-            if (student == null)
+            var studentToUpdate = db.Students.Find(id);
+            if (TryUpdateModel(studentToUpdate, "",
+               new string[] { "LastName", "FirstMidName", "EnrollmentDate" }))
             {
-                return HttpNotFound();
+                try
+                {
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                catch (DataException /* dex */)
+                {
+                    //Log the error (uncomment dex variable name and add a line here to write a log.
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
             }
-            return View(student);
+            return View(studentToUpdate);
         }
+
 
         // POST: Student/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+   /*     [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "ID,LastName,FirstName,EnrollmentDate")] Student student)
         {
@@ -98,12 +127,18 @@ namespace ContosoUniversity.Controllers
             return View(student);
         }
 
+        */
+
         // GET: Student/Delete/5
-        public ActionResult Delete(int? id)
+        public ActionResult Delete(int? id, bool? saveChangesError=false)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists, contact you system administrator.";
             }
             Student student = db.Students.Find(id);
             if (student == null)
@@ -113,16 +148,38 @@ namespace ContosoUniversity.Controllers
             return View(student);
         }
 
-        // POST: Student/Delete/5
+        // : Student/Delete/5
+        /*        [HttpPost, ActionName("Delete")]
+                [ValidateAntiForgeryToken]
+                public ActionResult DeleteConfirmed(int id)
+                {
+                    Student student = db.Students.Find(id);
+                    db.Students.Remove(student);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+        */
+
+        // : Student/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult Delete(int id)
         {
-            Student student = db.Students.Find(id);
-            db.Students.Remove(student);
-            db.SaveChanges();
+            try
+            {
+                Student student = db.Students.Find(id);
+                db.Students.Remove(student);
+                db.SaveChanges();
+            }
+            catch (DataException/* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+            }
             return RedirectToAction("Index");
         }
+
+
 
         protected override void Dispose(bool disposing)
         {

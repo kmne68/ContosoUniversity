bfb8f174fe9acc1f0c45a0b9362daa8c69694973
kmne68 <kmne68@gmail.com>
2016-11-13 02:09:05 -0600

    Finish add models/data

--- ContosoUniversity/DAL/SchoolInitializer.cs
+++ ContosoUniversity/DAL/SchoolInitializer.cs
@@ -26,13 +26,13 @@ namespace ContosoUniversity.DAL
             context.SaveChanges();
             var courses = new List<Course>
             {
-                new Course{courseID=1050,Title="Chemistry",Credits=3,},
-                new Course{courseID=4022,Title="Microeconomics",Credits=3,},
-                new Course{courseID=4041,Title="Macroeconomics",Credits=3,},
-                new Course{courseID=1045,Title="Calculus",Credits=4,},
-                new Course{courseID=3141,Title="Trigonometry",Credits=4,},
-                new Course{courseID=2021,Title="Composition",Credits=3,},
-                new Course{courseID=2042,Title="Literature",Credits=4,}
+                new Course{CourseID=1050,Title="Chemistry",Credits=3,},
+                new Course{CourseID=4022,Title="Microeconomics",Credits=3,},
+                new Course{CourseID=4041,Title="Macroeconomics",Credits=3,},
+                new Course{CourseID=1045,Title="Calculus",Credits=4,},
+                new Course{CourseID=3141,Title="Trigonometry",Credits=4,},
+                new Course{CourseID=2021,Title="Composition",Credits=3,},
+                new Course{CourseID=2042,Title="Literature",Credits=4,}
             };
 
             courses.ForEach(s => context.Courses.Add(s));
